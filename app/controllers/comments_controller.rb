86f9251e729430051d10c8ab30bb679f7b837d49
Joe Lyles <lylesjoe@protonmail.com>
2024-10-25 15:51:15 -0400

    Auth added to article and comments controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+      http_basic_authenticate_with name: "Joe", password: "password", except: [:index, :show]
+      
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
